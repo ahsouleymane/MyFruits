e1f620408a5071cbf440a3a022d155cc6372ed69
ABDOU SOULEYMANE <abdou.souleymane@ninetec.ne>
2024-05-18 09:44:48 +0100

    application vide

---
+++ MyFruits/Models/Fruit.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFruits.Models;
+
+public class Fruit
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public virtual Image Image { get; set; }
+
+    public decimal Price { get; set; }
+}
